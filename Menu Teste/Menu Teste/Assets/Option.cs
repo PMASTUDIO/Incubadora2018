969e35e17784448b384f188f2418924e2d2269d8
PMASTUDIO <32707772+PMASTUDIO@users.noreply.github.com>
2018-05-26 18:40:40 -0300

    HERP DERp
    Teste Menu :3

---
+++ Menu Teste/Menu Teste/Assets/Option.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Option : MonoBehaviour {
+   public  Sprite s;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+       
+	}
+
+    public void Click()
+        {
+            Image ar = gameObject.GetComponent<Image>();
+
+             ar.sprite = s;
+        }
+}
